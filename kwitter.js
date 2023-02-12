2dab50d2994ca2e570f45e05dc611624f5e6f104
NandhikaRanjith <89621311+NandhikaRanjith@users.noreply.github.com>
2023-02-11 20:01:31 -0600

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -3,3 +3,11 @@ function addUser(){
     localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
 }
+const firebaseConfig = {
+    apiKey: "AIzaSyBhbTo0Q4jgq6kfeFZQ4ls4zepS-GN1bOM",
+    authDomain: "let-s-chat-8a6f5.firebaseapp.com",
+    projectId: "let-s-chat-8a6f5",
+    storageBucket: "let-s-chat-8a6f5.appspot.com",
+    messagingSenderId: "627890575634",
+    appId: "1:627890575634:web:3b5b25b3ccabe3eaf7fbcb"
+  };
